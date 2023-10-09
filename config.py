aba6b34c39cf4616a1c2572964a9b841144b6584
Protullen <138602811+protullen@users.noreply.github.com>
2023-10-09 10:40:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,15 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 APP_ID = int(os.environ.get("APP_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 CHAT_ID = int(os.environ.get("CHAT_ID", ""))
-
+DB_URI = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+    
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
