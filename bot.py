850323e10fe549debbd2c554a5f0279cee75333a
Protullen <138602811+protullen@users.noreply.github.com>
2023-10-09 11:25:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,29 @@
-import sys
-from pyrogram import Client
-from pyrogram.enums import ParseMode
-from config import API_HASH, APP_ID, LOGGER, BOT_TOKEN
+from pyrogram import Client, enums
+from config import API_HASH, APP_ID, LOGGER, BOT_TOKEN 
 
-class Bot(Client):
+class Bot(Client): 
     def __init__(self):
         super().__init__(
-            name="Bot",
+            "bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            plugins={"root": "plugins"},
-            workers=8,
-            bot_token=BOT_TOKEN
+            plugins={
+                "root": "plugins"
+            },
+            workers=200,
+            bot_token=BOT_TOKEN,
+            sleep_threshold=10
         )
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        usr_bot_me = await self.get_me()
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Lx0980AI")
-        self.username = usr_bot_me.username
+        bot_details = await self.get_me()
+        self.set_parse_mode(enums.ParseMode.HTML)
+        self.LOGGER(__name__).info(
+            f"@{bot_details.username}  started! "
+        )
         
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
